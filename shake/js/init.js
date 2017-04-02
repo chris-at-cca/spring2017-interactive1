2482b3f8708a5725420040b1f6cf752748541c44
hamamoto <chamamoto@gmail.com>
2017-04-02 09:02:11 -0700

    Testing shake

---
+++ shake/js/init.js
@@ -0,0 +1,20 @@
+$( document ).ready(function() {
+
+    //create a new instance of shake.js.
+    var myShakeEvent = new Shake({
+        threshold: 15
+    });
+
+    // start listening to device motion
+    myShakeEvent.start();
+
+    // register a shake event
+    window.addEventListener('shake', shakeEventDidOccur, false);
+
+    //shake event callback
+    function shakeEventDidOccur () {
+
+        //put your own code here etc.
+        alert('Shake!');
+    }
+});
